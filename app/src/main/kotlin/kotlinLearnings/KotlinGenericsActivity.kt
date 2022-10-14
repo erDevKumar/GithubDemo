67420992595beffe1efc68b5ad5947bc445df533
Dev Kumar <erkumardevender@gmail.com>
2022-10-14 23:40:49 +0530

    Practice Problem on Basic Generics learning in Kotlin( refer class "KotlinGenericsActivity.kt")

--- app/src/main/kotlin/kotlinLearnings/KotlinGenericsActivity.kt
+++ app/src/main/kotlin/kotlinLearnings/KotlinGenericsActivity.kt
@@ -37,12 +37,12 @@ class KotlinGenericsActivity:AppCompatActivity() {
     inner class Species<SpeciesType:Any>(type:SpeciesType){
         //<SpeciesType:Any> was needed because we want to show "SpeciesType" as non null
         init{
-                Toast.makeText(this@KotlinGenericsActivity,"This is a:"+type.javaClass.simpleName, Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@KotlinGenericsActivity,"This is a:"+type.javaClass.simpleName/*+" named as"*/, Toast.LENGTH_SHORT).show()
                 //above line shows how we refer to context/activity that is parent
         }
     }
 
-    inner class Dog(name:String)
+    inner class Dog(val name:String)
     inner class Human(name:String)
 
 }
